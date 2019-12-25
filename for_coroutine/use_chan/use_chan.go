62426c21f191909aa193591d47d375aa4142901b
joeeyuan <joeeyuan@tencent.com>
2019-12-25 17:58:39 +0800

    update

---
+++ for_coroutine/use_chan/use_chan.go
@@ -0,0 +1,51 @@
+/**
+ * 并发编程，切片的线程安全性问题
+ */
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var list []int = []int{}
+var wgList sync.WaitGroup = sync.WaitGroup{}
+var muList sync.Mutex = sync.Mutex{}
+
+/*
+返回结果: 不加锁损失比较大，且随机损失
+list add num=10000
+list len=7553, time=2 ms
+new list add num=10000
+new list len=10000, time=1 ms
+
+list add num=10000
+list len=9230, time=2 ms
+new list add num=10000
+new list len=10000, time=5 ms
+*/
+func main() {
+	// 并发启动的协程数量
+	max := 10000
+	fmt.Printf("list add num=%d\n", max)
+
+	wgList.Add(max)
+	intChan := make(chan int , max)
+	time1 := time.Now().UnixNano()
+	for i := 0; i < max; i++ {
+		go func(c int){
+			intChan <- c
+			wgList.Done()
+		}(i)
+	}
+	wgList.Wait()
+	close(intChan)
+	time2 := time.Now().UnixNano()
+	// 注意list里面是地址还是值哦，地址就要新建变量
+	for i := range intChan{
+		list = append(list, i)
+	}
+	fmt.Printf("list len=%d, time=%d ms\n", len(list), (time2-time1)/1000000)
+	//fmt.Println(list)
+}
