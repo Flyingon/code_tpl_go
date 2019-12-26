2354725eacaf93466b08de1d8b30ddb299623f61
joeeyuan <joeeyuan@tencent.com>
2019-12-26 16:53:16 +0800

    update

--- for_channel/chan2slice/chan2Slice.go
+++ for_channel/chan2slice/chan2Slice.go
@@ -9,6 +9,7 @@ import (
 type ChanStruct struct {
 	Num  int
 	Data string
+	NumPtr *int
 }
 
 // 新数组地址全一样，不符合预期
@@ -16,20 +17,27 @@ func chan2SliceNotOK() {
 	// 定义chan
 	ptrChan := make(chan ChanStruct, 10)
 	for i := 1; i < 10; i++ {
+		num := i  // 注意，for循环取地址，都要新建变量，分配内存，否则i的地址都不会变，最后值只有最后一个
 		cs := ChanStruct{
 			Num:  i,
 			Data: strconv.Itoa(i),
+			NumPtr: &num,
 		}
 		ptrChan <- cs
 	}
 	close(ptrChan)
 	var csList []*ChanStruct
+	var NumPtrList []*int
 	// 从chan取数据，放入csList中
 	for c := range ptrChan {
 		csList = append(csList, &c)
+		NumPtrList = append(NumPtrList, c.NumPtr)
 	}
 	for _, cs := range csList {
 		fmt.Printf("cs: %p, %d, %s\n", cs, cs.Num, cs.Data)
+	}
+	for _, numPtr := range NumPtrList {
+		fmt.Printf("cs: %p, %d\n", numPtr, *numPtr)
 	}
 }
 
