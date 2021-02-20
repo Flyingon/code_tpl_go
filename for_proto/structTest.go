e2cabc0ac73afe335c6146c50c7353562670a7b4
joeeyuan <joeeyuan@tencent.com>
2021-02-20 11:46:57 +0800

    update

---
+++ for_proto/structTest.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	test "code_tpl_go/for_proto/proto"
+	"fmt"
+	"github.com/golang/protobuf/jsonpb"
+)
+
+// Marshaler jsonpb序列化结构体，可自己更改参数
+var Marshaler = jsonpb.Marshaler{EmitDefaults: true, OrigName: true, EnumsAsInts: true}
+
+// Unmarshaler jsonpb反序列化结构体，可自己更改参数
+var Unmarshaler = jsonpb.Unmarshaler{AllowUnknownFields: true}
+
+func main() {
+	res := test.Result{}
+	data := `{"JSON": {"a": 1, "b": "2", "c": {"c": "c"}}, "JSON2": {"a": 111111111122222222, "b": "2", "c": {"c": "c"}}}`
+	e := Unmarshaler.Unmarshal(bytes.NewReader([]byte(data)), &res)
+	//e := json.Unmarshal([]byte(data), &res)
+	fmt.Println(e, res.JSON2)
+	fmt.Println(uint64(res.JSON2["a"].GetNumberValue()))
+}
